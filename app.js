53306ff5dd79d3db5d4267d34f8b7f7e99776ccc
Rafal Czajkowski <78883568+xrttrx@users.noreply.github.com>
2022-03-28 12:03:20 +0200

    Add Portfolio Site

---
+++ app.js
@@ -0,0 +1,34 @@
+//Change active button and active section
+const sectBtn = document.querySelectorAll('.control')
+
+for (let i = 0; i < sectBtn.length; i++) {
+    sectBtn[i].addEventListener('click', () => {
+        document.querySelector('.active-btn').classList.remove('active-btn')
+        sectBtn[i].className += ' active-btn'
+        let id = sectBtn[i].dataset.id
+        document.querySelector('.active').classList.remove('active')
+        document.getElementById(id).classList.add('active')
+    })
+}
+
+//Toggle theme
+const html = document.querySelector('html')
+html.dataset.theme = 'dark'
+const themeBtn = document.querySelector('.theme-btn')
+themeBtn.addEventListener('click', () => {
+    if (html.dataset.theme == 'dark') {
+        html.dataset.theme = 'light'
+    } else {
+        html.dataset.theme = 'dark'
+    }
+})
+
+//Progress bars width - HTML, CSS, JS, React, Python
+
+let skill_perc = ['40%', '40%', '35%', '30%', '40%']
+skillbars = document.getElementsByClassName('skill')
+bartexts = document.getElementsByClassName('prog-text')
+for (let i in skill_perc) {
+    skillbars[i].style.width = skill_perc[i]
+    bartexts[i].innerHTML = skill_perc[i]
+}
