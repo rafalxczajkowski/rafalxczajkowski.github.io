28b94616ccd25a35a4837a1cbd3268b392864f2d
Rafal Czajkowski <rafalxczajkowski@hotmail.com>
2023-05-01 22:42:08 +0200

    skills changes

--- app.js
+++ app.js
@@ -2,13 +2,13 @@
 const sectBtn = document.querySelectorAll('.control')
 
 for (let i = 0; i < sectBtn.length; i++) {
-    sectBtn[i].addEventListener('click', () => {
-        document.querySelector('.active-btn').classList.remove('active-btn')
-        sectBtn[i].className += ' active-btn'
-        let id = sectBtn[i].dataset.id
-        document.querySelector('.active').classList.remove('active')
-        document.getElementById(id).classList.add('active')
-    })
+  sectBtn[i].addEventListener('click', () => {
+    document.querySelector('.active-btn').classList.remove('active-btn')
+    sectBtn[i].className += ' active-btn'
+    let id = sectBtn[i].dataset.id
+    document.querySelector('.active').classList.remove('active')
+    document.getElementById(id).classList.add('active')
+  })
 }
 
 //Toggle theme
@@ -16,19 +16,19 @@ const html = document.querySelector('html')
 html.dataset.theme = 'dark'
 const themeBtn = document.querySelector('.theme-btn')
 themeBtn.addEventListener('click', () => {
-    if (html.dataset.theme == 'dark') {
-        html.dataset.theme = 'light'
-    } else {
-        html.dataset.theme = 'dark'
-    }
+  if (html.dataset.theme == 'dark') {
+    html.dataset.theme = 'light'
+  } else {
+    html.dataset.theme = 'dark'
+  }
 })
 
-//Progress bars width - HTML, CSS, JS, React, Python, NumPy/Pandas, SQL, Django
+//Progress bars width - HTML, CSS, JS, React, Node, Git, Python, SQL
 
-let skill_perc = ['40%', '40%', '35%', '25%', '35%', '30%', '25%', '10%']
+let skill_perc = ['40%', '40%', '35%', '30%', '30%', '30%', '25%', '15%']
 skillbars = document.getElementsByClassName('skill')
 bartexts = document.getElementsByClassName('prog-text')
 for (let i in skill_perc) {
-    skillbars[i].style.width = skill_perc[i]
-    bartexts[i].innerHTML = skill_perc[i]
+  skillbars[i].style.width = skill_perc[i]
+  // bartexts[i].innerHTML = skill_perc[i]
 }
